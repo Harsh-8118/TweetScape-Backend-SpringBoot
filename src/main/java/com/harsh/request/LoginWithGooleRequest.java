b58b88fa65dbe350b1d148add7170b03de0673e0
Harsh Rajesh Raghamwar <harshraghamwar81@gmail.com>
2024-03-10 12:11:21 +0530

    TweetScape Backend Final Commit

---
+++ src/main/java/com/harsh/request/LoginWithGooleRequest.java
@@ -0,0 +1,11 @@
+package com.harsh.request;
+
+import lombok.Data;
+
+@Data
+public class LoginWithGooleRequest {
+	
+	private String credential;
+	private String clientId;
+
+}
---
+++ src/main/java/com/harsh/util/TweetUtil.java
@@ -0,0 +1,29 @@
+package com.harsh.util;
+
+import com.harsh.model.Like;
+import com.harsh.model.Twit;
+import com.harsh.model.User;
+
+public class TweetUtil {
+	
+	public final static boolean isLikedByReqUser(User reqUser, Twit twit) {
+		
+		for(Like like : twit.getLikes()) {
+			if (like.getUser().getId().equals(reqUser.getId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public final static boolean isRetwitedByReqUser(User reqUser, Twit twit) {
+		
+		for(User user : twit.getRetwitUser()) {
+			if (user.getId().equals(reqUser.getId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
